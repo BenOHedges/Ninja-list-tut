a50e28d522ae93d3b3745851494505b06a0cb784
BenOHedges <benjamin_hedges@yahoo.co.uk>
2021-02-07 01:29:07 +0000

    initial

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+const About = () => {
+	return (
+		<>
+			<Head>
+				<title>Ninja List | About</title>
+				<meta name="keywords" content="ninjas" />
+			</Head>
+			<div>
+				<h1>About</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+					Beatae quam soluta aliquid libero odio suscipit, porro
+					repellendus nulla distinctio impedit?
+				</p>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					Culpa, libero suscipit.
+				</p>
+			</div>
+		</>
+	);
+};
+
+export default About;
